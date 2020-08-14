28f9525f45466ef59b163f7627429b4faddfa5b0
Stefan Zander <stefan.zander@h-da.de>
2020-08-14 10:17:40 +0200

    added project for markdown slides

---
+++ smw/skript/marp/01_smw_intro.md
@@ -0,0 +1,38 @@
+---
+marp: true
+theme: custom-theme
+paginate: true
+---
+<style>
+/**
+ * @theme enable-all-auto-scaling
+ * @auto-scaling true
+ */
+
+/* @import 'default'; */
+/* @import url('user-theme2.css'); */
+
+
+
+</style>
+
+# Semantisches Wissensmanagement im Unternehmen: <br />Konzepte, Technologien, Anwendungen
+
+#### Prof. Dr. Stefan Linus Zander
+
+==Kapitel 1: Einführung in das Wissensmanagement== {.big}
+
+
+---
+<!-- header: Kapitel 1: Einführung -->
+<!-- footer: Prof. Dr. Stefan Zander -->
+# Leitfragen
+
+- Was ist Wissen ?
+- Warum benötigen Unternehmen ein funktionierendes Wissensmanagement ?
+- Welche Rolle spielt der Mensch im Wissensmanagement ?
+- Welche Wissensmanagementansätze gibt es und wodurch unterscheiden sie sich ?
+- Welche Rolle spielt IKT im Wissensmanagement?
+- Warum wird WM mit fortschreitender Digitalisierung immer wichtiger ?
+
+---
---
+++ smw/skript/marp/engine.js
@@ -0,0 +1,31 @@
+// engine.js
+const { Marp } = require('@marp-team/marp-core')
+
+const markdownItMark = require('markdown-it-mark')
+const markdownItAttrs = require('markdown-it-attrs')
+const markdownItContainer = require('markdown-it-container')
+const markdownItFootnote = require('markdown-it-footnote')
+const markdownItDiv = require('markdown-it-div')
+// const markdownIt = require('markdown-it')
+
+
+module.exports = (opts) => new Marp(opts)
+// .use(markdownIt)
+.use(markdownItMark)
+.use(markdownItAttrs)
+.use(markdownItContainer, 'definition')
+.use(markdownItContainer, 'task')
+.use(markdownItContainer, 'note')
+.use(markdownItContainer, 'columns')
+.use(markdownItContainer, '1st-col')
+.use(markdownItContainer, '2nd-col')
+.use(markdownItContainer, '3nd-col')
+// .use(markdownItFootnote)
+// .use(require('markdown-it-footnote'))
+.use(require('markdown-it-sup'))
+.use(markdownItDiv)
+.use(require('markdown-it-fontawesome'));
+
+// .use(require('markdown-it-mark'))
+// .use(require('markdown-it-container'), 'definition')
+// .use(require('markdown-it-footnote'))
