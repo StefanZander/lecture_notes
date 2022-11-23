14975795fe3eecd80a4f264a46886427e2c0cb3f
Stefan Zander <stefan.zander@h-da.de>
2022-11-23 11:07:14 +0100

    Create js_intro.js

---
+++ ewa/javascript/marp/js_intro.js
@@ -0,0 +1,20 @@
+/**
+
+Code Testbed for JavaScript
+
+*/
+
+
+let color1 = "blue";
+let color2 = color1;
+
+console.log(color1)
+console.log(color2)
+
+color1 = "red"
+
+console.log(color1)
+console.log(color2)
+
+
+console.log(color1.toUpperCase())
